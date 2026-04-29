3b0ba580ebe7c413faaa3f06b0dcb06c56ede553
Ilya <ilya909park@gmail.com>
2026-04-29 13:20:29 +0500

    pp2 tsis projects

--- tsis/phonebook tsis/config.py
+++ tsis/phonebook tsis/config.py
@@ -1,7 +1,7 @@
 # Database connection settings for PostgreSQL.
 # Change these values if your database name, password, host, or port is different.
 db_config = {
-    "dbname": "phonebook_db",
+    "dbname": "phonebook_db1",
     "user": "postgres",
     "password": "27082007",
     "host": "localhost",
